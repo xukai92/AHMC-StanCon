038705d6281b3d167751edb2f5578543ac297b06
Kai Xu <xukai921110@gmail.com>
2019-08-19 23:57:43 +0100

    add results for Gaussian

--- example.jl
+++ example.jl
@@ -1,26 +1,27 @@
 using AdvancedHMC
 
-n_samples, n_adapts, alpha_target = 10_000, 2_000, 0.8 # sampling parameter
+n_samples, n_adapts, target = 10_000, 2_000, 0.8 # sampling parameter
 q_init = randn(D)   # Draw a random starting points
 
 ### Building up NUTS
 
-metric = DiagEuclideanMetric(D)             # metric space
-h = Hamiltonian(metric, logdensity, grad)   # hamiltonian
-eps_init = find_good_eps(h, q_init)         # initial step size
-int = Leapfrog(eps_init)                    # integrator
-
+# Metric space
+metric = DiagEuclideanMetric(D)
+# Hamiltonian
+h = Hamiltonian(metric, logdensity, grad)
+# Initial step size
+eps_init = find_good_eps(h, q_init)
+# Integrator
+int = Leapfrog(eps_init)                    
 # Multinomial sampling with generalised no U-turn
 traj = NUTS{Multinomial,GeneralisedNoUTurn}(int)    
-
 # Stan's windowed adaptor
 adaptor = StanHMCAdaptor(
     n_adapts, 
     Preconditioner(metric), 
-    NesterovDualAveraging(alpha_target, eps_init)
+    NesterovDualAveraging(target, eps_init)
 )
 
 # Draw samples via simulating Hamiltonian dynamics
-# - `samples` will store the samples
-# - `stats` will store statistics for each sample
+# - `samples` will store the samples and `stats` will store statistics for each sample
 samples, stats = sample(h, traj, q_init, n_samples, adaptor, n_adapts)
