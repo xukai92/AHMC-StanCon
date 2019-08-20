0642d3d9539598b1ca2789af7fcb1ebeafbfae8b
Kai Xu <xukai921110@gmail.com>
2019-08-20 09:53:14 +0100

    tweak code

--- example.jl
+++ example.jl
@@ -1,20 +1,13 @@
 using AdvancedHMC
-
 n_samples, n_adapts, target = 10_000, 2_000, 0.8 # sampling parameter
 q_init = randn(D)   # Draw a random starting points
-
 ### Building up NUTS
-
 metric = DiagEuclideanMetric(D) # diagonal Euclidean metric space
 h = Hamiltonian(metric, logdensity_f, grad_f)   # hamiltonian on target distribution
 eps_init = find_good_eps(h, q_init) # initial step size
 int = Leapfrog(eps_init)    # Leapfrog integrator
-# Multinomial sampling with generalised no U-turn
-traj = NUTS{Multinomial,GeneralisedNoUTurn}(int)    
-# Stan's windowed adaptor
-adaptor = StanHMCAdaptor(
+traj = NUTS{Multinomial,GeneralisedNoUTurn}(int)    # Multinomial sampling with generalised no U-turn   
+adaptor = StanHMCAdaptor(   # Stan's windowed adaptor
     n_adapts, Preconditioner(metric), NesterovDualAveraging(target, eps_init)
 )
-
-# Draw samples via simulating Hamiltonian dynamics
-samples, stats = sample(h, traj, q_init, n_samples, adaptor, n_adapts)
+samples, stats = sample(h, traj, q_init, n_samples, adaptor, n_adapts)  # draw samples
