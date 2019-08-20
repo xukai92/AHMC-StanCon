16ab3e17ae20a627a7ce77ea23db0ff481bed69d
Kai Xu <xukai921110@gmail.com>
2019-08-20 22:08:55 +0100

    Updates from Overleaf

---
+++ lr.jl
@@ -0,0 +1,10 @@
+@model LR(x, y, Nd, Nc) = begin
+    B ~ MvNormal(zeros(Nc), 10)
+    B0 ~ Normal(0, 10)
+    sigma ~ Truncated(Cauchy(0, 5), 0, Inf)
+    mu = B0 .+ x * B
+    y ~ MvNormal(mu, sigma)
+end
+
+x, y, Nd, Nc = ... # load data
+chain = sample(LR(x, y, Nd, Nc), NUTS(2_000, 1_000, 0.8))
