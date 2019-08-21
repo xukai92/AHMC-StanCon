427fe0942c182f33178fe5ddc6be3977edfda396
Kai Xu <xukai921110@gmail.com>
2019-08-21 15:19:41 +0100

    h -> H

--- example.jl
+++ example.jl
@@ -3,7 +3,7 @@ n_samples, n_adapts, target = 10_000, 2_000, 0.8 # set up sampling parameters
 q_init = randn(D)   # draw a random starting point
 ### Building up NUTS
 metric = DiagEuclideanMetric(D) # diagonal Euclidean metric space
-h = Hamiltonian(metric, logdensity_f, grad_f)   # hamiltonian on the target distribution
+h = Hamiltonian(metric, logdensity_f, grad_f)   # Hamiltonian on the target distribution
 eps_init = find_good_eps(h, q_init) # initial step size
 int = Leapfrog(eps_init)    # leapfrog integrator
 traj = NUTS{Multinomial,GeneralisedNoUTurn}(int)    # multinomial sampling with generalised no U-turn   
